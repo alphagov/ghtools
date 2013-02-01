ab2ecb084e360e5da2cfe49d189f01a71ebda9a7
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-02-01 17:18:10 +0000

    Update teams if they already exist.

--- ghtools/command/migrate_teams.py
+++ ghtools/command/migrate_teams.py
@@ -21,7 +21,7 @@ def migrate_teams(args):
 			if len(row[1].strip()) > 0:
 				members[row[0]] = row[1]
 
-	dst_teams = [t['name'] for t in dst.client.get('/orgs/{0}/teams'.format(dst.organisation)).json]
+	dst_teams = dict((t['name'], t['id']) for t in dst.client.get('/orgs/{0}/teams'.format(dst.organisation)).json)
 
 	for src_team in src.client.get('/orgs/{0}/teams'.format(src.organisation)).json:
 		if src_team['name'] in dst_teams:
@@ -33,29 +33,30 @@ def migrate_teams(args):
 			}
 
 			dst_team = dst.client.post('/orgs/{0}/teams'.format(dst.organisation), data=payload).json
+			dst_teams[src_team['name']] = dst_team['id']
 
-			for src_member in src.client.get('/teams/{0}/members'.format(src_team['id'])).json:
-				if src_member['login'] in members:
-					try:
-						dst.client.put('/teams/{0}/members/{1}'.format(dst_team['id'], members[src_member['login']]), data=' ')
-						log.debug("User '{0}' added to '{1}'".format(src_member['login'], dst_team['name']))
-					except APIError as e:
-						if e.response.status_code == 404:
-							log.error("User '{0}' not found.".format(members[src_member['login']]))
-						else:
-							raise
-				else:
-					log.error("Failed to migrate member '{0}' to team '{1}'".format(src_member['login'], src_team['name']))
-
-			for src_repo in src.client.get('/teams/{0}/repos'.format(src_team['id'])).json:
+		for src_member in src.client.get('/teams/{0}/members'.format(src_team['id'])).json:
+			if src_member['login'] in members:
 				try:
-					dst.client.put('/teams/{0}/repos/{1}'.format(dst_team['id'], dst.full_name(src_repo['name'])), data=' ')
-					log.debug("Project '{0}' to '{1}'".format(src_repo['name'], dst_team['name']))
+					dst.client.put('/teams/{0}/members/{1}'.format(dst_teams[src_team['name']], members[src_member['login']]), data=' ')
+					log.debug("User '{0}' added to '{1}'".format(src_member['login'], src_team['name']))
 				except APIError as e:
 					if e.response.status_code == 404:
-						log.debug("Project '{0}' does not exist".format(src_repo['name']))
+						log.error("User '{0}' not found.".format(members[src_member['login']]))
 					else:
 						raise
+			else:
+				log.error("Failed to migrate member '{0}' to team '{1}'".format(src_member['login'], src_team['name']))
+
+		for src_repo in src.client.get('/teams/{0}/repos'.format(src_team['id'])).json:
+			try:
+				dst.client.put('/teams/{0}/repos/{1}'.format(dst_teams[src_team['name']], dst.full_name(src_repo['name'])), data=' ')
+				log.debug("Project '{0}' to '{1}'".format(src_repo['name'], src_team['name']))
+			except APIError as e:
+				if e.response.status_code == 404:
+					log.debug("Project '{0}' does not exist".format(src_repo['name']))
+				else:
+					raise
 
 
 
