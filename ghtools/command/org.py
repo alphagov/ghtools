435a728a4d2d488b24a2d56dba4ee349de60fdfc
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2013-02-04 12:17:21 +0000

    Remove nickname option from gh-org command

--- ghtools/command/org.py
+++ ghtools/command/org.py
@@ -5,33 +5,34 @@ import logging
 
 from argh import *
 from ghtools.api import GithubAPIClient
+from ghtools.identifier import Identifier
 
 log = logging.getLogger(__name__)
 parser = ArghParser(description="Interact with a GitHub organisation")
-parser.add_argument('-n', '--nickname', default='public', help='GitHub instance nickname')
-parser.add_argument('org', help='Organisation name')
+parser.add_argument('org', help='Organisation identifier (e.g. rails, enterprise:mycorp)')
+parser.add_argument('-j', '--json', default=False, help='Print full JSON representations')
 
 
-@arg('-j', '--json', default=False, help='Print full JSON representations')
 def repos(args):
     """
     Print a list of organisation repositories
     """
-    c = GithubAPIClient(nickname=args.nickname)
-    for repo in c.paged_get('/orgs/{0}/repos'.format(args.org)):
+    ident = Identifier.from_string(args.org)
+    c = GithubAPIClient(nickname=ident.github)
+    for repo in c.paged_get('/orgs/{0}/repos'.format(ident.org)):
         if args.json:
             print(json.dumps(repo, indent=2))
         else:
             print(repo['name'])
 
 
-@arg('-j', '--json', default=False, help='Print full JSON representations')
 def members(args):
     """
     Print a list of organisation members
     """
-    c = GithubAPIClient(nickname=args.nickname)
-    for repo in c.paged_get('/orgs/{0}/members'.format(args.org)):
+    ident = Identifier.from_string(args.org)
+    c = GithubAPIClient(nickname=ident.github)
+    for repo in c.paged_get('/orgs/{0}/members'.format(ident.org)):
         if args.json:
             print(json.dumps(repo, indent=2))
         else:
