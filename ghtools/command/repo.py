dd04430fe7b01dc3385381527b3079956060ab5d
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2013-02-04 12:13:27 +0000

    Remove nickname option from gh-repo command

--- ghtools/command/repo.py
+++ ghtools/command/repo.py
@@ -5,20 +5,21 @@ import logging
 
 from argh import *
 from ghtools.api import GithubAPIClient
+from ghtools.identifier import Identifier
 
 log = logging.getLogger(__name__)
 parser = ArghParser(description="Interact with GitHub repos")
-parser.add_argument('-n', '--nickname', default='public', help='GitHub instance nickname')
-parser.add_argument('repo', help='Repo name, e.g. defunkt/resque')
+parser.add_argument('repo', help='Repo identifier, e.g. defunkt/resque, enterprise:mycorp/myproj')
 
 
 def delete(args):
     """
     Delete the specified repository
     """
-    c = GithubAPIClient(nickname=args.nickname)
+    ident = Identifier.from_string(args.repo)
+    c = GithubAPIClient(nickname=ident.github)
 
-    res = c.delete('/repos/{0}'.format(args.repo))
+    res = c.delete('/repos/{0}/{1}'.format(ident.org, ident.repo))
     res.raise_for_status()
 
 
@@ -26,9 +27,10 @@ def get(args):
     """
     Print the JSON representation of the specified repository to STDOUT
     """
-    c = GithubAPIClient(nickname=args.nickname)
+    ident = Identifier.from_string(args.repo)
+    c = GithubAPIClient(nickname=ident.github)
 
-    res = c.get('/repos/{0}'.format(args.repo))
+    res = c.get('/repos/{0}/{1}'.format(ident.org, ident.repo))
     return json.dumps(res.json, indent=2)
 
 
