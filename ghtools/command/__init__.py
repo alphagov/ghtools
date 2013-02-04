4f903a6b974fafba7c4a17b41e0a75b6b62209b3
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2013-02-04 13:16:16 +0000

    Change default loglevel to INFO

--- ghtools/command/__init__.py
+++ ghtools/command/__init__.py
@@ -3,7 +3,7 @@ from os import environ as env
 
 __all__ = []
 
-_log_level_default = logging.WARN
+_log_level_default = logging.INFO
 _log_level = getattr(logging, env.get('GHTOOLS_LOGLEVEL', '').upper(), _log_level_default)
 
 logging.basicConfig(format='%(levelname)s: %(message)s', level=_log_level)
