a915e5617dda8406f219241b9a37d246f71d09f4
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2013-02-06 13:33:48 +0000

    Improve coverage of Organisation class

--- ghtools/github/organisation.py
+++ ghtools/github/organisation.py
@@ -9,9 +9,12 @@ class Organisation(object):
         self.org = self._ident.org
         self.client = client or make_client(self._ident)
 
-    def get_repo(self, repo):
-        url = '/repos/{0}/{1}'.format(self.org, repo)
-        res = self.client.get(url)
+    def add_team_member(self, team, login):
+        res = self.client.put('/teams/{0}/members/{1}'.format(team['id'], login), data=' ')
+        return res.json()
+
+    def add_team_repo(self, team, repo_name):
+        res = self.client.put('/teams/{0}/repos/{1}'.format(team['id'], repo_name), data=' ')
         return res.json()
 
     def create_repo(self, repo):
@@ -26,6 +29,15 @@ class Organisation(object):
         ]
         payload = dict((k, repo[k]) for k in keys)
         res = self.client.post('/orgs/{0}/repos'.format(self.org), data=payload)
+        return res.json()
+
+    def create_team(self, team):
+        res = self.client.post('/orgs/{0}/teams'.format(self.org), data=team)
+        return res.json()
+
+    def get_repo(self, repo):
+        url = '/repos/{0}/{1}'.format(self.org, repo)
+        res = self.client.get(url)
         return res.json()
 
     def list_members(self):
@@ -43,14 +55,6 @@ class Organisation(object):
     def list_team_repos(self, team):
         return self.client.paged_get('/teams/{0}/repos'.format(team['id']))
 
-    def create_team(self, team):
-        return self.client.post('/orgs/{0}/teams'.format(self.org), data=team)
-
-    def add_team_member(self, team_id, login):
-        return self.client.put('/teams/{0}/members/{1}'.format(team_id, login), data=' ')
-
-    def add_team_repo(self, team_id, repo_name):
-        return self.client.put('/teams/{0}/repos/{1}'.format(team_id, repo_name), data=' ')
 
     def __str__(self):
         return '<Organisation {0}>'.format(self._ident)
--- test/unit/github/test_organisation.py
+++ test/unit/github/test_organisation.py
@@ -12,22 +12,24 @@ class TestOrganisation(object):
         self.patcher = patch('ghtools.github.organisation.make_client')
         self.mock_client_cons = self.patcher.start()
         self.mock_client = self.mock_client_cons.return_value
+        self.o = Organisation('foo')
 
     def teardown(self):
         self.patcher.stop()
 
-    def test_get_repo(self):
-        o = Organisation('foo')
-        o.get_repo('bar')
-        self.mock_client.get.assert_called_with('/repos/foo/bar')
+    def test_add_team_member(self):
+        self.mock_client.put.return_value.json.return_value = {'id': 456}
+        res = self.o.add_team_member({'id': 123}, 'joebloggs')
+        self.mock_client.put.assert_called_with('/teams/123/members/joebloggs', data=' ')
+        assert_equal(res, {'id': 456})
 
-    def test_get_repo_nickname(self):
-        o = Organisation('enterprise:foo')
-        o.get_repo('bar')
-        self.mock_client.get.assert_called_with('/repos/foo/bar')
+    def test_add_team_repo(self):
+        self.mock_client.put.return_value.json.return_value = {'id': 456}
+        res = self.o.add_team_repo({'id': 123}, 'myproj')
+        self.mock_client.put.assert_called_with('/teams/123/repos/myproj', data=' ')
+        assert_equal(res, {'id': 456})
 
     def test_create_repo(self):
-        o = Organisation('foo')
         repo = {
             'name': 'reponame',
             'description': 'My description',
@@ -42,8 +44,43 @@ class TestOrganisation(object):
         repo_passed = repo.copy()
         del repo_passed['not_included']
 
-        o.create_repo(repo)
+        self.o.create_repo(repo)
         self.mock_client.post.assert_called_with('/orgs/foo/repos', data=repo_passed)
+
+    def test_create_team(self):
+        self.mock_client.post.return_value.json.return_value = {'id': 456}
+        res = self.o.create_team({'id': 123})
+        self.mock_client.post.assert_called_with('/orgs/foo/teams', data={'id': 123})
+        assert_equal(res, {'id': 456})
+
+    def test_get_repo(self):
+        self.o.get_repo('bar')
+        self.mock_client.get.assert_called_with('/repos/foo/bar')
+
+    def test_get_repo_nickname(self):
+        o = Organisation('enterprise:foo')
+        o.get_repo('bar')
+        self.mock_client.get.assert_called_with('/repos/foo/bar')
+
+    def test_list_members(self):
+        self.o.list_members()
+        self.mock_client.paged_get.assert_called_with('/orgs/foo/members')
+
+    def test_list_repos(self):
+        self.o.list_repos()
+        self.mock_client.paged_get.assert_called_with('/orgs/foo/repos')
+
+    def test_list_teams(self):
+        self.o.list_teams()
+        self.mock_client.paged_get.assert_called_with('/orgs/foo/teams')
+
+    def test_list_team_members(self):
+        self.o.list_team_members({'id': 123})
+        self.mock_client.paged_get.assert_called_with('/teams/123/members')
+
+    def test_list_team_repos(self):
+        self.o.list_team_repos({'id': 123})
+        self.mock_client.paged_get.assert_called_with('/teams/123/repos')
 
     def test_str(self):
         o1 = Organisation('foo')
