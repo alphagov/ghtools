b0a96098d3021ff245c911550a3a9f0179330281
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2013-02-06 18:01:32 +0000

    Improve coverage of api module

--- test/unit/test_api.py
+++ test/unit/test_api.py
@@ -1,6 +1,10 @@
 from ..helpers import *
 
-from ghtools.api import GithubAPIClient, envkey
+import os
+
+from ghtools.api import GithubAPIClient, DEFAULT_GITHUB
+from ghtools.api import envkey
+from ghtools.exceptions import GithubError
 
 # This environment variable is expected to be set when testing. Tox will set
 # it (see tox.ini)
@@ -23,6 +27,26 @@ class TestGithubAPIClient(RequestMockTestCase):
     def logout(self):
         self.register_response('get', API_ROOT + '/authorizations', 404)
 
+    def test_default_to_default_github(self):
+        c = GithubAPIClient()
+        assert_equal(c.nickname, DEFAULT_GITHUB)
+
+    def test_explicitly_set_root(self):
+        c = GithubAPIClient('https://github.mycorp/api/v3', nickname='mycorp')
+        assert_equal(c.nickname, 'mycorp')
+        assert_equal(c.root, 'https://github.mycorp/api/v3')
+
+    def test_raise_if_unknown_github(self):
+        with assert_raises(GithubError) as e:
+            GithubAPIClient(nickname='unknown')
+            assert_regexp_matches(r'No known API root', e.message)
+
+    def test_use_specified_ca_bundle(self):
+        os.environ['GITHUB_TEST_CA_BUNDLE'] = '/usr/share/openssl/bundle.pem'
+        c = GithubAPIClient(nickname='test')
+        assert_equal(c._session.verify, '/usr/share/openssl/bundle.pem')
+        del os.environ['GITHUB_TEST_CA_BUNDLE']
+
     def test_login_should_set_token(self):
         c = GithubAPIClient(nickname='test')
         assert_equal(c.token, None)
@@ -30,10 +54,25 @@ class TestGithubAPIClient(RequestMockTestCase):
         assert_equal(c.token, 'abc123')
         assert_true(c.logged_in)
 
+    def test_request(self):
+        self.register_response('options', API_ROOT + '/foo', 200, {}, 'foo')
+        c = GithubAPIClient(nickname='test')
+        assert_equal(c.request('options', '/foo').content, 'foo')
+
+    def test_delete(self):
+        self.register_response('delete', API_ROOT + '/bat', 204, {}, '')
+        c = GithubAPIClient(nickname='test')
+        assert_equal(c.delete('/bat').status_code, 204)
+
     def test_get(self):
         self.register_response('get', API_ROOT + '/foo', 200, {}, 'foo')
         c = GithubAPIClient(nickname='test')
         assert_equal(c.get('/foo').content, 'foo')
+
+    def test_patch(self):
+        self.register_response('patch', API_ROOT + '/foo', 200, {}, 'foo')
+        c = GithubAPIClient(nickname='test')
+        assert_equal(c.patch('/foo').content, 'foo')
 
     def test_post(self):
         self.register_response('post', API_ROOT + '/bar', 201, {}, 'bar')
@@ -45,10 +84,18 @@ class TestGithubAPIClient(RequestMockTestCase):
         c = GithubAPIClient(nickname='test')
         assert_equal(c.put('/baz').json(), {'message': 'success'})
 
-    def test_delete(self):
-        self.register_response('delete', API_ROOT + '/bat', 204, {}, '')
-        c = GithubAPIClient(nickname='test')
-        assert_equal(c.delete('/bat').status_code, 204)
+    def test_paged_get(self):
+        self.register_response('get', API_ROOT + '/paged', 200,
+                               {'link': '<{0}/paged?page=2>; rel="next"'.format(API_ROOT)},
+                               '[{"item": 1}, {"item": 2}]')
+        self.register_response('get', API_ROOT + '/paged?page=2', 200, {}, '[{"item": 3}]')
+        client = GithubAPIClient(nickname='test')
+        res = client.paged_get('/paged')
+        assert_equal(res.next(), {'item': 1})
+        assert_equal(res.next(), {'item': 2})
+        assert_equal(res.next(), {'item': 3})
+        with assert_raises(StopIteration):
+            res.next()
 
 
 def test_envkey():
